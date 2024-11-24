6f8f3c779a644a02a8c60531ffabd9dede987cca
Zailta <mannanbhat60@gmail.com>
2024-11-24 20:49:01 +0530

    Multiple DS creation - final changes

--- src/main/java/com/jpa/app/configurations/HotelEntityDatatSourceConfiguration.java
+++ src/main/java/com/jpa/app/configurations/HotelEntityDatatSourceConfiguration.java
@@ -5,11 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.zaxxer.hikari.util.DriverDataSource;
 
-
+@Configuration
 public class HotelEntityDatatSourceConfiguration {
 	
 	@ConfigurationProperties(prefix = "spring.datasource.hotel")
@@ -17,7 +19,6 @@ public class HotelEntityDatatSourceConfiguration {
 	public DataSourceProperties HotelEntityDatatSourceProperties() {
 		return new  DataSourceProperties();
 	}
-	
 	@Bean
 	public DataSource HotelEntityDatatSource() {
 		return HotelEntityDatatSourceProperties().initializeDataSourceBuilder().build();
--- src/main/java/com/jpa/app/configurations/UserEntityDatatSourceConfiguration.java
+++ src/main/java/com/jpa/app/configurations/UserEntityDatatSourceConfiguration.java
@@ -5,11 +5,12 @@ import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.zaxxer.hikari.util.DriverDataSource;
 
-
+@Configuration
 public class UserEntityDatatSourceConfiguration {
 	
 	@ConfigurationProperties(prefix = "spring.datasource.user")
