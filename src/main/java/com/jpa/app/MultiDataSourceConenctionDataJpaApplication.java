f5094d112f57bbcfdfd445d82d2754abf068d20b
Zailta <mannanbhat60@gmail.com>
2024-11-24 18:12:32 +0530

    Initial Commit Data JPA

---
+++ src/main/java/com/jpa/app/MultiDataSourceConenctionDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiDataSourceConenctionDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiDataSourceConenctionDataJpaApplication.class, args);
+	}
+
+}
